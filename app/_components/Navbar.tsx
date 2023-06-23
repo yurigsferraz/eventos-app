9eedae7190bf9b089fee56636c11e54923d562c7
Yuri Ferraz <yurigsf@dcc.ufrj.br>
2023-06-23 16:39:54 -0300

    Muda nome da logo

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
-            <a href="/" className="text-white font-bold text-lg">Logo</a>
+            <a href="/" className="text-white font-bold text-lg">EventOS</a>
           </div>
           <div className="flex">
             <a href="/about" className="text-gray-300 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium">About</a>
